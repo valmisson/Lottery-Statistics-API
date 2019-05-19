c3ff5392f878b12e241f41d138ea7ce331060bab
Valmisson Grizorte <missongrizorte@gmail.com>
2019-05-19 00:47:58 -0300

    adding firebase

---
+++ src/service/firebase.js
@@ -0,0 +1,12 @@
+const admin = require('firebase-admin')
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL
+  }),
+  databaseURL: process.env.FIREBASE_DATABASE_URL
+})
+
+module.exports = admin.database()
