cfd27ef01f95a8ec9dda9e21ebb7939a0e6b4f08
Valmisson Grizorte <missongrizorte@gmail.com>
2019-05-20 17:04:37 -0300

    creating controller to update all results

--- src/controller/Result.js
+++ src/controller/Result.js
@@ -15,4 +15,48 @@ const SaveResult = async (req, res) => {
   }
 }
 
-module.exports = { SaveResult }
+// Update all results
+const UpdateResult = async (req, res) => {
+  try {
+    const { lottery } = req.params
+    const resultsList = [] // should be an array of objects with results
+
+    if (resultsList.length === 0) throw new Error(`Error ao atualizar resultados da ${lottery}, lista de resultados esta vazio`)
+
+    const results = resultsList.map(result => {
+      const { numero, data, dezenas } = result.concurso
+
+      const premiacaoArray = Object.entries(result.concurso.premiacao)
+
+      const premiacao = premiacaoArray.map(premio => {
+        let nome = premio[0].charAt(0).toUpperCase() + premio[0].slice(1)
+        nome = nome.replace(/Acertos_/, '')
+
+        return {
+          nome,
+          ganhadores: premio[1].ganhadores || '0',
+          valor: premio[1].valor_pago
+        }
+      })
+
+      return {
+        concurso: {
+          numero,
+          data
+        },
+        dezenas,
+        premiacao
+      }
+    })
+
+    results.forEach(async result => {
+      await save({ lottery, result })
+    })
+
+    res.json({ message: `Resultados da ${lottery} atualizado!` })
+  } catch (error) {
+    return res.sendError(error.message, 500)
+  }
+}
+
+module.exports = { SaveResult, UpdateResult }
--- src/routes/Result.js
+++ src/routes/Result.js
@@ -1,6 +1,7 @@
 const route = require('express').Router()
-const { SaveResult } = require('@controller/Result')
+const { SaveResult, UpdateResult } = require('@controller/Result')
 
 route.get('/:lottery/salvar', SaveResult)
+route.get('/:lottery/atualizar', UpdateResult)
 
 module.exports = route
