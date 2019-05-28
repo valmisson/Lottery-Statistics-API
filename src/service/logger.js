c378e8e42b46d3d945f6d06cb1013f1069a8a7f9
Valmisson Grizorte <missongrizorte@gmail.com>
2019-05-28 17:14:33 -0300

    displaying error message at server console

--- src/service/logger.js
+++ src/service/logger.js
@@ -1,10 +1,14 @@
 const { createLogger, format, transports } = require('winston')
+const dateFormate = require('dateformat')
+
+const date = dateFormate(new Date(), 'dd-mm-yyyy, H:MM:ss - "UTC"o')
 
 const logger = createLogger({
   level: 'info',
   format: format.json(),
-  defaultMeta: { date: new Date() },
+  defaultMeta: { date },
   transports: [
+    new transports.Console(),
     new transports.File({ filename: 'logs/error.log', level: 'error' }),
     new transports.File({ filename: 'logs/all.log' })
   ]
